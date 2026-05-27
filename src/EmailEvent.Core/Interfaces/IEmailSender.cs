570cb857adef027f51ef5a3f29c339f795db15db
Alnils <Albin.nilsson.se@gmail.com>
2026-05-27 15:36:05 +0200

    created interface to send message

---
+++ src/EmailEvent.Core/Interfaces/IEmailSender.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmailEvent.Core.Interfaces;
+
+public interface IEmailSender
+{
+    Task SendVerificationCodeAsync(string email, string code, CancellationToken ct);
+}
