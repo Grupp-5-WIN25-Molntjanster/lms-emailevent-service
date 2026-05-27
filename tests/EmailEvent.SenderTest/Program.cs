e295ac7244fe1b8834c006ae16105bc866597c12
Alnils <Albin.nilsson.se@gmail.com>
2026-05-27 16:26:30 +0200

    created sender test for service bus

---
+++ tests/EmailEvent.SenderTest/Program.cs
@@ -0,0 +1,26 @@
+﻿using Azure.Messaging.ServiceBus;
+using Microsoft.Extensions.Configuration;
+
+
+var config = new ConfigurationBuilder()
+    .AddJsonFile("appsettings.json")
+    .Build();
+
+
+var connectionString = config["Azure:ServiceBus:ConnectionString"]
+    ?? throw new InvalidOperationException("Missing ConnectionString");
+
+var queueName = config["Azure:ServiceBus:QueueName"]
+    ?? throw new InvalidOperationException("Missing QueueName");
+
+
+var email = args.Length > 0 ? args[0] : "wkwwd78046@minitts.net";
+var code = args.Length > 1 ? args[1] : "13371337";
+
+var messageBody = $$"""{"email":"{{email}}","code":"{{code}}"}""";
+
+await using var client = new ServiceBusClient(connectionString);
+ServiceBusSender sender = client.CreateSender(queueName);
+await sender.SendMessageAsync(new ServiceBusMessage(messageBody));
+
+Console.WriteLine($"Sent verification code {code} to {email}");
